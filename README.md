849ad09e3afa8d6c100a3699b11bca1596fbea11
jnorthr <james.b.northrop@googlemail.com>
2021-04-09 15:30:06 +0100

    Initial Commit of sample Micronaut Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello3
 Test Build of Simple Micronaut Project
+
+- [Micronaut Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
---
+++ src/test/java/Hello3/Hello3Test.java
@@ -0,0 +1,21 @@
+package Hello3;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class Hello3Test {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
